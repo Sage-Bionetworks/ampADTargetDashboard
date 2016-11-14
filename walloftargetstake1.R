23e543b896e7d30d9bdbfa5d437538c8f487e266
Benjamin Logsdon <aeberhart@gmail.com>
2016-11-14 11:02:55 -0800

    ben ic

---
+++ walloftargetstake1.R
@@ -0,0 +1,17 @@
+#push target list to synapse, share with Kenny
+
+#map to ensembl id
+
+#figure out which network to pull down
+
+#extract local neighborhoods
+
+#create custom edge list 
+
+#share with Kenny
+
+#pull down expression for local neighborhoods
+
+#pull down clinical assessment for patients
+
+#share with kenny appropriately
