23590358f18a3b4a493188431f8c54bc6fa5fa15
kdaily <kdaily@users.noreply.github.com>
2017-02-14 11:57:46 -0800

    use key config json file

--- global.R
+++ global.R
@@ -7,8 +7,10 @@ library(igraph)
 library(forcats)
 library(ggplot2)
 library(stringr)
+library(rjson)
 
-synapseLogin()
+config <- rjson::fromJSON(file="key.json")
+synapseLogin(username=config$userName, apiKey = config$apiKey)
 
 oddiStatusColors <- c("good"="#5e933f", "medium"="#ef7d0b", "bad"="#a30b0d", "unknown"="#a3a3a3")
 lillyStatusColors <- c("3"="green", "2"="yellow", "1"="orange", "0"="red", "NA"="red", "unk"="red")
