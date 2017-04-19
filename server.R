ecba3a73a5e938d0d5321e1387316a9f87e24196
kdaily <kenneth.daily@sagebase.org>
2017-04-19 16:10:56 -0700

    change legend position

--- server.R
+++ server.R
@@ -112,7 +112,7 @@ shinyServer(function(input, output, session) {
       p <- ggplot(tmp, aes(x=hgnc_symbol, y=fpkm))
       p <- p + geom_boxplot(aes(fill=`Cognitive Diagnosis`))
       p <- p + scale_fill_manual(values=wes_palette("Chevalier"))
-      p <- p + theme_bw()
+      p <- p + theme_bw() + theme(legend.position="bottom")
       p
     })
     
