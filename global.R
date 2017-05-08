01c7e162763e6424699c3b4aca0b42022acd8a4d
kdaily <kenneth.daily@sagebase.org>
2017-05-08 14:02:39 -0700

    fix color for unk

--- global.R
+++ global.R
@@ -10,7 +10,7 @@ library(stringr)
 library(rjson)
 
 oddiStatusColors <- c("good"="#5e933f", "medium"="#ef7d0b", "bad"="#a30b0d", "unknown"="#a3a3a3")
-lillyStatusColors <- c("3"="#5e933f", "2"="yellow", "1"="#ef7d0b", "0"="#a30b0d", "NA"="#a3a3a3", "unk"="a3a3a3")
+lillyStatusColors <- c("3"="#5e933f", "2"="yellow", "1"="#ef7d0b", "0"="#a30b0d", "NA"="#a3a3a3", "unk"="#a3a3a3")
 
 targetManifsetSketch <- htmltools::withTags(table(
   class = 'display',
