d8e17d6388ea4958c2036bc85b86e94a748d5f0c
kdaily <kdaily@users.noreply.github.com>
2018-01-25 12:59:27 -0800

    change to use AD score data and not druggability

--- process.R
+++ process.R
@@ -5,52 +5,16 @@ library(feather)
 synapseLogin()
 
 wotFolderId <- 'syn7525089'
-druggabilityDataId <- 'syn7555804'
+scoreDataId <- 'syn11688680'
 targetListOrigId <- "syn8656625"
 
-
-druggabilityDataOutputFile <- "./druggabilityData.csv"
 targetListOutputFile <- "./targetList.csv"
 targetListDistinctOutputFile <- "./targetListDistinct.csv"
 targetManifestOutputFile <- "./targetManifest.csv"
 
-druggabilityData <- synGet(druggabilityDataId) %>% 
-  getFileLocation() %>% 
-  read_csv() %>%
-  select(-Center, -DDI_Interest, -some_number, -GENE_DESCRIPTION) %>%
-  rename(ensembl.gene=ENSG) %>%
-  mutate(status_assays=forcats::fct_recode(status_assays, unknown=""),
-         status_crystal_structure=forcats::fct_recode(status_crystal_structure, unknown=""),
-         status_pocket=forcats::fct_recode(status_pocket, unknown=""),
-         status_in_vivo_work=forcats::fct_recode(status_in_vivo_work, unknown=""),
-         status_known_ligands=forcats::fct_recode(status_known_ligands, unknown=""),
-         Lilly_DrugEBIlity_Consensus_Score=forcats::fct_explicit_na(as.character(Lilly_DrugEBIlity_Consensus_Score),
-                                                           na_level = "unk"),
-         `Lilly_GW_Druggability_Structure-based`=forcats::fct_explicit_na(as.character(`Lilly_GW_Druggability_Structure-based`),
-                                                                 na_level = "unk")) %>% 
-  group_by(GENE_SYMBOL) %>% 
-  slice(1) %>% 
-  ungroup()
-
-druggabilityData <- druggabilityData %>%
-  select(GENE_SYMBOL, starts_with('status')) %>%
-  distinct() %>%
-  tidyr::gather(category, status, starts_with('status')) %>%
-  mutate(status_numeric=forcats::fct_recode(status, `0`="unknown", `0`='bad',
-                                   `1`="medium", `2`="good"),
-         status_numeric=levels(status_numeric)[as.numeric(status_numeric)]) %>%
-  select(GENE_SYMBOL, status_numeric) %>%
-  group_by(GENE_SYMBOL) %>%
-  summarize(sum_status=sum(as.numeric(status_numeric))) %>%
-  ungroup() %>%
-  right_join(druggabilityData, by=c('GENE_SYMBOL')) %>%
-  arrange(GENE_SYMBOL)
-
-write_csv(druggabilityData, druggabilityDataOutputFile)
-
-fDrugData <- synStore(File(druggabilityDataOutputFile, 
-                           parentId=wotFolderId), 
-                      used=druggabilityDataId, forceVersion=FALSE)
+scoreData <- synGet(scoreDataId) %>% 
+  getFileLocation() %>% read_csv() %>% 
+  rename(ensembl.gene=gene, Score=adDriverScore, Gene=external_gene_name)
 
 ####### Process target list
 targetListOrig <- synGet(targetListOrigId) %>% 
@@ -81,18 +45,15 @@ fTargetListDistinct <- synStore(File(targetListDistinctOutputFile,
                              parentId=wotFolderId), 
                         used=fTargetList, forceVersion=FALSE)
 
-targetManifest <- targetListDistinct %>%
-  left_join(druggabilityData, by=c('Gene'='GENE_SYMBOL',
-                                   'ensembl.gene'='ensembl.gene')) %>%
-  select(Gene,
-         Centers,
-         nominations) %>%
-  distinct() %>%
-  arrange(-nominations)
+targetManifest <- scoreData %>%
+  filter(Gene %in% targetListDistinct$Gene) %>% 
+  select(Gene, Score) %>% 
+  mutate(Score=round(Score, 1)) %>% 
+  arrange(-Score)
 
 write_csv(targetManifest, targetManifestOutputFile)
 
 fTargetManifest <- synStore(File(targetManifestOutputFile, 
                                      parentId=wotFolderId), 
-                                used=c(fTargetListDistinct, fDrugData), 
+                                used=c(fTargetListDistinct, scoreDataId), 
                             forceVersion=FALSE)
